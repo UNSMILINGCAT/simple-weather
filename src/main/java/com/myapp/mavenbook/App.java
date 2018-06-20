aabfd02474ce054fa182ba7362594109711fad87
不笑猫 <867974171@qq.com>
2018-06-20 21:41:12 +0800

    第一次提交，初始化

---
+++ src/main/java/com/myapp/mavenbook/App.java
@@ -0,0 +1,13 @@
+package com.myapp.mavenbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
